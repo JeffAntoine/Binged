d362dbcc84185c8537e1dd2f826cb307b658c988
JeffAntoine <Janto031@fiu.edu>
2018-03-03 07:38:39 -0500

    initial start

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFyFjbAaNHpXVS8h7QL5pO_qRyikCq9nE",
+  authDomain: "binged-9a469.firebaseapp.com",
+  databaseURL: "https://binged-9a469.firebaseio.com",
+  storageBucket: ""
+};
---
+++ src/views/pages/sign-in/sign-in-page.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { authActions } from 'src/auth';
+import Button from 'src/views/components/button';
+
+import './sign-in-page.css';
+
+
+const SignInPage = ({signInWithGithub, signInWithGoogle}) => {
+  return (
+    <div className="g-row sign-in">
+      <div className="g-col">
+        <h1 className="sign-in__heading" >Sign in</h1>
+        <Button className="sign-in__button" onClick={signInWithGithub}>GitHub</Button>
+        <Button className="sign-in__button" onClick={signInWithGoogle}>Google</Button>
+      </div>
+    </div>
+  );
+};
+
+SignInPage.propTypes = {
+  signInWithGithub: PropTypes.func.isRequired,
+  signInWithGoogle: PropTypes.func.isRequired
+};
+
+
+//=====================================
+//  CONNECT
+//-------------------------------------
+
+const mapDispatchToProps = {
+  signInWithGithub: authActions.signInWithGithub,
+  signInWithGoogle: authActions.signInWithGoogle
+};
+
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(SignInPage)
+);
